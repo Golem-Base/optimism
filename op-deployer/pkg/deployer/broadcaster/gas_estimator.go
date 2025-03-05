d6d80eb18a47e43e28d4455305b37fecdce8b30c
Lorenzo Manacorda <lorenzo@mailbox.org>
2025-03-05 14:16:49 +0100

    bump limits

--- op-deployer/pkg/deployer/broadcaster/gas_estimator.go
+++ op-deployer/pkg/deployer/broadcaster/gas_estimator.go
@@ -12,12 +12,12 @@ var (
 	// baseFeePadFactor = 50% as a divisor
 	baseFeePadFactor = big.NewInt(2)
 	// tipMulFactor = 5 as a multiplier
-	tipMulFactor = big.NewInt(5)
+	tipMulFactor = big.NewInt(10)
 	// dummyBlobFee is a dummy value for the blob fee. Since this gas estimator will never
 	// post blobs, it's just set to 1.
 	dummyBlobFee = big.NewInt(1)
 	// maxTip is the maximum tip that can be suggested by this estimator.
-	maxTip = big.NewInt(50 * 1e9)
+	maxTip = big.NewInt(100 * 1e9)
 	// minTip is the minimum tip that can be suggested by this estimator.
 	minTip = big.NewInt(1 * 1e9)
 )
